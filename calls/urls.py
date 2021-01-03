10de94452c118d17b2072c64d8b5cd3863748c66
Peter <peter@pbwis.com>
2021-01-03 15:50:29 +0000

    connect <a href> to detail page

--- calls/urls.py
+++ calls/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
+app_name = 'calls'
+
 urlpatterns = [
-    path('', views.calls_list)
+    path('', views.calls_list),
+    path('<slug>/', views.call_detail, name='detail')
 ]
--- calls/views.py
+++ calls/views.py
@@ -5,3 +5,8 @@ from .models import Call
 def calls_list(request):
     calls = Call.objects.all()
     return render(request, 'calls/calls_list.html', {'calls': calls})
+
+
+def call_detail(request, slug):
+    call = Call.objects.get(slug=slug)
+    return render(request, 'calls/call_detail.html', {'call': call})
