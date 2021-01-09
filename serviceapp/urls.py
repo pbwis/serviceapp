d10f2ddf4540aafbacf047a04660cb268fb99f80
Peter <peter@pbwis.com>
2021-01-09 17:52:46 +0000

    Sign in and Sign up + view

--- serviceapp/urls.py
+++ serviceapp/urls.py
@@ -3,13 +3,14 @@ from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
+from calls import views as call_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', views.home),
-    path('calls/', include('calls.urls'))
+    # path('', views.home),
+    path('calls/', include('calls.urls')),
+    path('', call_views.calls_list, name='home'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
