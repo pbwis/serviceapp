c711c836b9559ccc574f80bbf30fcf108b354896
Peter <peter@pbwis.com>
2021-02-04 18:15:44 +0000

    Printer details

--- calls/urls.py
+++ calls/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('<slug>/delete/', views.call_delete, name='delete'),
     path('printer/<slug>/create', views.call_create, name='create'),
     path('customer/<slug>/create/', views.printer_create, name='printer_create'),
+    path('printer/<slug>/', views.printer_detail, name='printer_detail'),
     path('customer/create/', views.customer_create, name='customer_create'),
     path('customer/', views.customer_list, name='customer_list'),
     path('customer/<slug>/', views.customer_detail, name='customer_detail'),
